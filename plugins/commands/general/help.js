d0352564c7b39a5c4edf4304347e5ca3200c9a58
armiayasis3 <padrigalmia35@gmail.com>
2025-05-02 15:16:37 +0800

    Update help.js

--- plugins/commands/general/help.js
+++ plugins/commands/general/help.js
@@ -1,132 +1,73 @@
 const config = {
     name: "help",
-    _name: {
-        "ar_SY": "الاوامر"
-    },
     aliases: ["cmds", "commands"],
     version: "1.0.3",
     description: "Show all commands or command details",
     usage: "[command] (optional)",
-    credits: "XaviaTeam"
-}
-
-const langData = {
-    "en_US": {
-        "help.list": "{list}\n\n⇒ Total: {total} commands\n⇒ Use {syntax} [command] to get more information about a command.",
-        "help.commandNotExists": "Command {command} does not exists.",
-        "help.commandDetails": `
-            ⇒ Name: {name}
-            ⇒ Aliases: {aliases}
-            ⇒ Version: {version}
-            ⇒ Description: {description}
-            ⇒ Usage: {usage}
-            ⇒ Permissions: {permissions}
-            ⇒ Category: {category}
-            ⇒ Cooldown: {cooldown}
-            ⇒ Credits: {credits}
-        `,
-        "0": "Member",
-        "1": "Group Admin",
-        "2": "Bot Admin"
-    },
-    "vi_VN": {
-        "help.list": "{list}\n\n⇒ Tổng cộng: {total} lệnh\n⇒ Sử dụng {syntax} [lệnh] để xem thêm thông tin về lệnh.",
-        "help.commandNotExists": "Lệnh {command} không tồn tại.",
-        "help.commandDetails": `
-            ⇒ Tên: {name}
-            ⇒ Tên khác: {aliases}
-            ⇒ Phiên bản: {version}
-            ⇒ Mô tả: {description}
-            ⇒ Cách sử dụng: {usage}
-            ⇒ Quyền hạn: {permissions}
-            ⇒ Thể loại: {category}
-            ⇒ Thời gian chờ: {cooldown}
-            ⇒ Người viết: {credits}
-        `,
-        "0": "Thành viên",
-        "1": "Quản trị nhóm",
-        "2": "Quản trị bot"
-    },
-    "ar_SY": {
-        "help.list": "{list}\n\n⇒ المجموع: {total} الاوامر\n⇒ يستخدم {syntax} [امر] لمزيد من المعلومات حول الأمر.",
-        "help.commandNotExists": "امر {command} غير موجود.",
-        "help.commandDetails": `
-            ⇒ اسم: {name}
-            ⇒ اسم مستعار: {aliases}
-            ⇒ وصف: {description}
-            ⇒ استعمال: {usage}
-            ⇒ الصلاحيات: {permissions}
-            ⇒ فئة: {category}
-            ⇒ وقت الانتظار: {cooldown}
-            ⇒ الاعتمادات: {credits}
-        `,
-        "0": "عضو",
-        "1": "إدارة المجموعة",
-        "2": "ادارة البوت"
-    }
-}
+    credits: "XaviaTeam/Liane/coffee"
+};
 
 function getCommandName(commandName) {
-    if (global.plugins.commandsAliases.has(commandName)) return commandName;
-
-    for (let [key, value] of global.plugins.commandsAliases) {
-        if (value.includes(commandName)) return key;
+    if (global.plugins.commandsAliases.has(commandName)) {
+        return commandName;
     }
-
-    return null
+    return Array.from(global.plugins.commandsAliases).find(([, aliases]) => aliases.includes(commandName))?.[0] || null;
 }
 
-async function onCall({ message, args, getLang, userPermissions, prefix }) {
+async function onCall({ message, args, userPermissions, prefix }) {
     const { commandsConfig } = global.plugins;
     const commandName = args[0]?.toLowerCase();
 
     if (!commandName) {
-        let commands = {};
         const language = data?.thread?.data?.language || global.config.LANGUAGE || 'en_US';
+        const commands = {};
+
         for (const [key, value] of commandsConfig.entries()) {
-            if (!!value.isHidden) continue;
-            if (!!value.isAbsolute ? !global.config?.ABSOLUTES.some(e => e == message.senderID) : false) continue;
-            if (!value.hasOwnProperty("permissions")) value.permissions = [0, 1, 2];
-            if (!value.permissions.some(p => userPermissions.includes(p))) continue;
-            if (!commands.hasOwnProperty(value.category)) commands[value.category] = [];
-            commands[value.category].push(value._name && value._name[language] ? value._name[language] : key);
+            // Check for command visibility and permissions
+            if (value.isHidden || (value.isAbsolute && !global.config?.ABSOLUTES.includes(message.senderID)) || !value.permissions?.some(p => userPermissions.includes(p))) {
+                continue;
+            }
+            const category = commands[value.category] || (commands[value.category] = []);
+            category.push(`- ${value._name?.[language] || key}`);
         }
 
-        let list = Object.keys(commands)
-            .map(category => `⌈ ${category.toUpperCase()} ⌋\n${commands[category].join(", ")}`)
-            .join("\n\n");
+        // Automatically group by categories but without enforcing any particular order
+        const commandList = Object.keys(commands)
+            .map(category => `
+╭─╼━━━━━━━━╾─╮
+│  ${category}
+│ ${commands[category].join("\n│ ")}
+╰─━━━━━━━━━╾─╯`)
+            .join("");
 
-        message.reply(getLang("help.list", {
-            total: Object.values(commands).map(e => e.length).reduce((a, b) => a + b, 0),
-            list,
-            syntax: message.args[0].toLowerCase()
-        }));
-    } else {
-        const command = commandsConfig.get(getCommandName(commandName, commandsConfig));
-        if (!command) return message.reply(getLang("help.commandNotExists", { command: commandName }));
+        return message.reply(`
+━━━━━━━━━━━━━━━━
+𝙰𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜:
+${commandList}
+Chat -𝚑𝚎𝚕𝚙 <command name>
+𝚃𝚘 𝚜𝚎𝚎 𝚑𝚘𝚠 𝚝𝚘 𝚞𝚜𝚎 
+𝚊𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜.
 
-        const isHidden = !!command.isHidden;
-        const isUserValid = !!command.isAbsolute ? global.config?.ABSOLUTES.some(e => e == message.senderID) : true;
-        const isPermissionValid = command.permissions.some(p => userPermissions.includes(p));
-        if (isHidden || !isUserValid || !isPermissionValid)
-            return message.reply(getLang("help.commandNotExists", { command: commandName }));
-
-        message.reply(getLang("help.commandDetails", {
-            name: command.name,
-            aliases: command.aliases.join(", "),
-            version: command.version || "1.0.0",
-            description: command.description || '',
-            usage: `${prefix}${commandName} ${command.usage || ''}`,
-            permissions: command.permissions.map(p => getLang(String(p))).join(", "),
-            category: command.category,
-            cooldown: command.cooldown || 3,
-            credits: command.credits || ""
-        }).replace(/^ +/gm, ''));
+𝙴𝚡𝚊𝚖𝚙𝚕𝚎: .help gemini
+━━━━━━━━━━━━━━━━
+`);
     }
+
+    const command = commandsConfig.get(getCommandName(commandName));
+    if (!command || command.isHidden || (command.isAbsolute && !global.config?.ABSOLUTES.includes(message.senderID)) || !command.permissions.some(p => userPermissions.includes(p))) {
+        return message.reply(`Command ${commandName} does not exist or you do not have permission to access it.`);
+    }
+
+    message.reply(`
+━━━━━━━━━━━━━━━━
+𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝙽𝚊𝚖𝚎: ${command.name}
+𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description || 'No description provided.'}
+𝚄𝚜𝚊𝚐𝚎: ${prefix}${commandName} ${command.usage || ''}
+━━━━━━━━━━━━━━━━
+    `.replace(/^ +/gm, ''));
 }
 
 export default {
     config,
-    langData,
     onCall
-}
+};
